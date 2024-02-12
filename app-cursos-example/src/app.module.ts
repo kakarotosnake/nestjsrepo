2f1dc85cb89a6c94e76c833f9d0149d39590a592
japs <japs8704@gmail.com>
2024-02-12 14:23:18 -0600

    update with data base connection

--- app-cursos-example/src/app.module.ts
+++ app-cursos-example/src/app.module.ts
@@ -8,6 +8,8 @@ import { AwardsModule } from './awards/awards.module';
 import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 
 @Module({
@@ -20,7 +22,9 @@ import { join } from 'path';
         rootPath:join(__dirname,'..','public'),
       }
     ),
-    CoursesModule, AuthModule, VideosModule, AwardsModule],
+    MongooseModule.forRoot(process.env.DB_URI),
+    CoursesModule, AuthModule, VideosModule, AwardsModule, UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ app-cursos-example/src/awards/model/awards.schema.ts
@@ -0,0 +1,24 @@
+import {Prop, Schema , SchemaFactory} from "@nestjs/mongoose";
+import mongoose, { Document } from "mongoose";
+
+export type AwardsDocument= Awards & Document;
+
+@Schema()
+export class Awards{
+
+    @Prop()
+    title: string;
+
+    @Prop()
+    idUser:mongoose.Types.ObjectId;
+
+    @Prop()
+    description: string;
+
+   
+
+
+   
+}
+
+export const AwardsSchema=SchemaFactory.createForClass(Awards);
